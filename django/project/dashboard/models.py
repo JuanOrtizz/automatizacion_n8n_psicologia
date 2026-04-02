e0af2675e3c0e95f5a937d81dc4e8b804ed7392a
JuanOrtizz <juan_ignaortiz@outlook.com>
2026-04-02 02:14:24 -0300

    feat: Agrego modelo 'Sesiones'

--- django/project/dashboard/models.py
+++ django/project/dashboard/models.py
@@ -1,2 +1,10 @@
 from django.db import models
 
+class Sesiones(models.Model):
+    nombre = models.CharField(max_length=100)
+    dia_preferido = models.CharField(max_length=20)
+    fecha_solicitud = models.DateField(auto_now_add=True)
+    telefono = models.CharField(max_length=25)
+
+    class Meta:
+        db_table = 'sesiones'

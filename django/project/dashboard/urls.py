4d82b418ac1b4263f37c10f2aab03d8a53a13c4b
JuanOrtizz <juan_ignaortiz@outlook.com>
2026-04-03 00:07:32 -0300

    feat: Agrego vista y enrutamiento para 'register_sesion'

--- django/project/dashboard/urls.py
+++ django/project/dashboard/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns= [
     path('', views.dashboard, name='dashboard'),
+    path('register_sesion/', views.registrar_sesion, name='register_sesion'),
 ]
--- django/project/dashboard/views.py
+++ django/project/dashboard/views.py
@@ -1,4 +1,13 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from .forms import SesionesForm
 
 def dashboard(request):
     return render(request, 'dashboard/index.html')
+
+def registrar_sesion(request):
+    if request.method == 'POST':
+        form = SesionesForm(request.POST)
+    else:
+        form = SesionesForm()
+    return render(request, 'dashboard/register_sesion.html', {'form': form})

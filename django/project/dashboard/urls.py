16d3783b8a2b3edca79c3488836f90c81a9b7d3d
JuanOrtizz <juan_ignaortiz@outlook.com>
2026-04-04 02:48:52 -0300

    feat: Agrego y actualizo nomenclaturas de rutas en urls.py de app dashboard

--- django/project/dashboard/urls.py
+++ django/project/dashboard/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns= [
     path('', views.dashboard, name='dashboard'),
-    path('register_sesion/', views.registrar_sesion, name='register_sesion'),
+    path('registrar-sesion/', views.registrar_sesion, name='registrar-sesion'),
+    path('modificar-sesion/<int:sesion_id>/', views.modificar_sesion, name='modificar-sesion'),
+    path('eliminar-sesion/<int:sesion_id>/', views.eliminar_sesion, name='eliminar-sesion'),
 ]

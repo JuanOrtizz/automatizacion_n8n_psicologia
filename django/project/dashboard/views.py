40e02c82148336e6187077ff44731cf27924223b
JuanOrtizz <juan_ignaortiz@outlook.com>
2026-04-04 02:53:22 -0300

    feat: Agrego vistas Modificar_sesion, Eliminar_sesion y actualizo vistas registrar_sesion y dashboard

--- django/project/dashboard/views.py
+++ django/project/dashboard/views.py
@@ -2,37 +2,55 @@ from django.http import JsonResponse
 from django.shortcuts import render
 from .forms import SesionesForm
 from .models import Sesiones
+from .services import register_sesion, get_sesion, get_sesions, update_sesion, delete_sesion
+
 
 def dashboard(request):
-    sesiones = Sesiones.objects.all()
     #Agregar filtros
-    return render(request, 'dashboard/index.html', {'sesiones': sesiones})
+    sesions = get_sesions()
+    return render(request, 'dashboard/index.html', {'sesions': sesions})
 
 def registrar_sesion(request):
+    titulo_form = "Registrar Sesión"
     if request.method == 'POST':
         form = SesionesForm(request.POST)
         if form.is_valid():
-            # capturo los datos del formulario
-            nombre = form.cleaned_data['nombre'].strip()
-            dia_preferido = form.cleaned_data['dia_preferido']
-            fecha_solicitud = form.cleaned_data['fecha_solicitud']
-            telefono = form.cleaned_data['telefono'].strip()
-
-            # verifico si ya existe una sesión para ese cliente (nombre + teléfono)
-            if Sesiones.objects.filter(nombre = nombre, telefono = telefono).exists():
-                return JsonResponse({'success': False, 'errors': 'Ya existe una sesión para ese cliente'})
-
-            # creo la nueva sesion y la guardo en la base de datos
-            nueva_sesion = Sesiones(
-                nombre=nombre,
-                dia_preferido=dia_preferido,
-                fecha_solicitud=fecha_solicitud,
-                telefono=telefono
-            )
-            nueva_sesion.save()
-            return JsonResponse({'success': True, 'message': 'Sesión registrada con éxito.'})
+            try:
+                register_sesion(form.cleaned_data)
+                return JsonResponse({'success': True, 'message': 'Sesión registrada con éxito'})
+            except ValueError as e:
+                return JsonResponse({'success': False, 'errors': str(e)})
         else:
             return JsonResponse({'success': False, 'message': 'Datos inválidos', 'errors': form.errors})
     else:
         form = SesionesForm()
-    return render(request, 'dashboard/register_sesion.html', {'form': form})
+    return render(request, 'dashboard/create_update_sesion.html', {'form': form, 'titulo_form': titulo_form})
+
+def modificar_sesion(request, sesion_id):
+    titulo_form = "Modificar Sesión"
+    sesion = get_sesion(sesion_id)
+    if request.method == 'POST':
+        form = SesionesForm(request.POST)
+        if form.is_valid():
+            try:
+                update_sesion(sesion, form.cleaned_data)
+                return JsonResponse({'success': True, 'message': 'Sesión modificada con éxito'})
+            except ValueError as e:
+                return JsonResponse({'success': False, 'errors': str(e)})
+    else:
+        form = SesionesForm(initial={
+            'nombre': sesion.nombre,
+            'dia_preferido': sesion.dia_preferido,
+            'fecha_solicitud': sesion.fecha_solicitud,
+            'telefono': sesion.telefono
+        })
+
+    return render(request, 'dashboard/create_update_sesion.html', {'form': form, 'titulo_form': titulo_form, 'sesion_id': sesion_id})
+
+def eliminar_sesion(request, sesion_id):
+    if request.method == 'POST':
+        try:
+            delete_sesion(sesion_id)
+            return JsonResponse({'success': True, 'message': 'Sesión eliminada con éxito'})
+        except ValueError as e:
+            return JsonResponse({'success': False, 'errors': str(e)})

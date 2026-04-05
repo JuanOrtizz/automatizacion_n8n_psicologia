fd339a05ab36c072b00bb4388228ad29f2114770
JuanOrtizz <juan_ignaortiz@outlook.com>
2026-04-05 03:25:15 -0300

    refactor: Actualizo urls.py del proyecto con las urls de la nueva app accounts_auth

--- django/project/project/urls.py
+++ django/project/project/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts_auth/', include('accounts_auth.urls')),
     path('', include('dashboard.urls')),
 ]

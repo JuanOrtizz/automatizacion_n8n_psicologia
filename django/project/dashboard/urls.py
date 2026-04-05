1b594b86110b1cf0264562e876f3eed7d480b3b9
JuanOrtizz <juan_ignaortiz@outlook.com>
2026-04-05 02:03:23 -0300

    refactor: Actualizo urls.py para incluir rutas de API v1 (dashboard)

--- django/project/dashboard/urls.py
+++ django/project/dashboard/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns= [
@@ -6,4 +6,8 @@ urlpatterns= [
     path('registrar-sesion/', views.registrar_sesion, name='registrar-sesion'),
     path('modificar-sesion/<int:sesion_id>/', views.modificar_sesion, name='modificar-sesion'),
     path('eliminar-sesion/<int:sesion_id>/', views.eliminar_sesion, name='eliminar-sesion'),
+]
+
+urlpatterns += [
+    path('api/v1/', include('dashboard.api.v1.urls')),
 ]

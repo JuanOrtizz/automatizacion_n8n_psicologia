f6c372270e7a206952d30fde00eb5a8536385c1f
JuanOrtizz <juan_ignaortiz@outlook.com>
2026-04-05 13:45:55 -0300

    feat: Agrego rutas API v1 para tokens JWT y users

---
+++ django/project/accounts_auth/api/v1/urls.py
@@ -0,0 +1,15 @@
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.urls import path
+
+from .views import UserViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='accounts_auth')
+
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
+
+urlpatterns += router.urls

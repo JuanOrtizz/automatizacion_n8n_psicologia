e74e3f0d2571b2d2c3b41531b83a3622dc818980
JuanOrtizz <juan_ignaortiz@outlook.com>
2026-04-11 17:21:01 -0300

    feat: Registro modelos Sesiones y UsuariosAutorizados en panel admin django

--- django/project/accounts_auth/admin.py
+++ django/project/accounts_auth/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import UsuariosAutorizados
 
-# Register your models here.
+@admin.register(UsuariosAutorizados)
+class UsuariosAutorizadosAdmin(admin.ModelAdmin):
+    list_display = ['id', 'email', 'fecha_autorizacion']
+    readonly_fields = ['fecha_autorizacion']
--- django/project/dashboard/admin.py
+++ django/project/dashboard/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Sesiones
 
-# Register your models here.
+@admin.register(Sesiones)
+class SesionesAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nombre', 'telefono', 'fecha_solicitud', 'dia_sesion', 'horario_sesion']
+    readonly_fields = ['fecha_solicitud']
